57f56de8a2d7566dadae8c5572fea6489fa09fd6
ALEXIA-DENISA BLAGA <alexia.denisa.blaga@stud.ubbcluj.ro>
2026-01-14 18:13:25 +0200

    Lab 10

---
+++ Models/BatchPredictionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using static Blaga_Alexia_Denisa_Lab4.PricePredictionModel;
+
+namespace Blaga_Alexia_Denisa_Lab4.Models
+{
+    
+    
+        public class BatchPredictionViewModel
+        {
+            public IFormFile? File { get; set; }
+            // Rezultatele (predicțiile) pentru fiecare rând din fișier
+            public List<ModelOutput>? Predictions { get; set; }
+            public string? ErrorMessage { get; set; }
+        }
+    
+}
